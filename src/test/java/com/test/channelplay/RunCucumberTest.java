17f646d0b87cc6044a95c70522a1be7f3fbd7b4b
pritamAtta <123435412+pritamAtta@users.noreply.github.com>
2023-04-19 23:01:37 +0530

    Update RunCucumberTest.java

--- src/test/java/com/test/channelplay/RunCucumberTest.java
+++ src/test/java/com/test/channelplay/RunCucumberTest.java
@@ -7,8 +7,8 @@ import org.testng.annotations.DataProvider;
 		features = "src/test/resources/com/test/channelplay/feature",
 		glue = {"com.test.channelplay.stepDefinition"},
 		tags = "@currentFile",
-		format= {"pretty","html:test-output_1","json:target/cucumber-reports/CucumberTestReport.json"},
-		plugin = {"pretty", "rerun:target/rerun.txt", "timeline:target/timeline", "json:target/cucumber-reports/CucumberTestReport.json"}, monochrome=true
+		plugin = {"pretty", "rerun:target/rerun.txt", "timeline:target/timeline", "json:target/cucumber.json"}, 
+	        monochrome=true
 		)
 
 public class RunCucumberTest extends AbstractTestNGCucumberTests{
